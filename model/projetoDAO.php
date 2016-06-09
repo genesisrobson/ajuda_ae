2d86ce44c7ee126a30244f8f2863ffce51dccb98
genesis_robson1990 <genesis.rssantos@gmail.com>
2016-06-09 05:15:51 +0000

    alteradas as classes de deletar com o () no lugar do (Projeto ), foi tirado tambem o getNome do bind_param

--- model/projetoDAO.php
+++ model/projetoDAO.php
@@ -7,7 +7,7 @@ class ProjetoDAO{
             echo "Falha no MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
         }
         $stmt = $mysqli->prepare("INSERT INTO tabela_vaquinha(nome,propositor,objetivo,cidade,estado,vlMeta,deadline) VALUES (?,?,?,?,?,?,?)");
-        $stmt->bind_param("sssssfs",$p->getNome(),$p->getPropositor(),$p->getObjetivo(),$p->getCidade(),$p->getEstado(),$p->getvlMeta(),$p->getDeadline());
+        $stmt->bind_param("sssssds",$p->getNome(),$p->getPropositor(),$p->getObjetivo(),$p->getCidade(),$p->getEstado(),$p->getvlMeta(),$p->getDeadline());
         if (!$stmt->execute()) {
             echo "Erro: (" . $stmt->errno . ") " . $stmt->error . "<br>";
         }
@@ -24,6 +24,21 @@ class ProjetoDAO{
         $proj = new Projeto($id,$nome, $propositor, $objetivo, $cidade, $estado, $vlMeta, $deadline);
         return $proj;
     }
+    
+    public function deletar($id){
+        $mysqli = new mysqli("127.0.0.1", "genesis_robson19", "", "ajuda_ae");
+        if ($mysqli->connect_errno) {
+            echo "Falha no MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+        }
+        $stmt = $mysqli->prepare("DELETE FROM tabela_vaquinha WHERE id=?");
+        $stmt->bind_param("i",$id);
+        if (!$stmt->execute()) {
+            echo "Erro: (" . $stmt->errno . ") " . $stmt->error . "<br>";
+        }
+        $stmt->close();
+    }
+    
+
     //$stmt->bind_result($col1, $col2);
 }
 ?>
